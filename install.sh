2ac48fcf3c418fa0b1a6e63459eae37fb775b199
Denys <den.polischuk19@gmail.com>
2021-07-03 12:13:51 +0200

    script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+SERVICE_NAME="fn-lock-toggle"
+SCRIPT_PATH="/usr/bin/$SERVICE_NAME.sh"
+SERVICE_PATH="/lib/systemd/system/$SERVICE_NAME.service"
+if [[ $1 == 'uninstall' ]]; then
+  systemctl stop $SERVICE_NAME
+  systemctl disable $SERVICE_NAME
+  rm $SERVICE_PATH 
+  rm $SCRIPT_PATH
+  exit 0
+fi
+SERVICE="
+[Unit]\n
+Description=Toggles fn lock on start\n
+\n
+[Service]\n
+Type=simple\n
+ExecStart=/bin/bash $SCRIPT_PATH\n
+\n
+[Install]\n
+WantedBy=multi-user.target\n
+"
+
+SCRIPT="
+#!/bin/bash\n
+while [[ ! -d /sys/module/hid\_apple/parameters/ ]]; do sleep 1s; done\n
+echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
+"
+
+echo -e $SCRIPT > $SCRIPT_PATH
+echo -e $SERVICE > $SERVICE_PATH
+systemctl start fn-lock-toggle
+systemctl enable fn-lock-toggle
